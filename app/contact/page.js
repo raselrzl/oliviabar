45de01f7e73faf63190fb0694c060bdc728a10a8
Rasel Miah <rasel6041@gmail.com>
2024-12-12 14:08:59 +0100

    Fix open pdf

--- app/contact/page.js
+++ app/contact/page.js
@@ -385,8 +385,7 @@ export default function ContactPage() {
             Öppettider
           </h2>
           <div className="p-4 text-center  rounded-lg">
-          <p className="text-xs text-white">Tisdag 16:00-00:00</p>
-            <p className="text-xs text-white">Onsdag - Fredag 16:00 - 01:00</p>
+            <p className="text-xs text-white">Tisdag - Fredag 16:00 - 01:00</p>
             <p className="text-xs text-white">Lördag - 15:00 - 01:00</p>
             <p className="text-xs text-white">Söndag & Måndag - Stängt</p>
           </div>
